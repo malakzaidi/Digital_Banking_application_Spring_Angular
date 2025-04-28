30fe102d8d415b9c9db285f0c26f480b8a1b9cd6
Malak Zaidi <malakzaidi815@gmail.com>
2025-04-28 15:13:04 +0000

    create the entities and enums + create the mappings and relations between classes

--- src/main/java/org/springmvc/ebanking/entities/AccountOperation.java
+++ src/main/java/org/springmvc/ebanking/entities/AccountOperation.java
@@ -1,4 +1,23 @@
 package org.springmvc.ebanking.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springmvc.ebanking.enums.OperationType;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class AccountOperation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    @Enumerated(EnumType.STRING)
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+    private String description;
 }
--- src/main/java/org/springmvc/ebanking/entities/CurrentAccount.java
+++ src/main/java/org/springmvc/ebanking/entities/CurrentAccount.java
@@ -1,4 +1,15 @@
 package org.springmvc.ebanking.entities;
 
-public class CurrentAccount {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@DiscriminatorValue("CA")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class CurrentAccount extends BankAccount {
+    private double overDraft;
 }
