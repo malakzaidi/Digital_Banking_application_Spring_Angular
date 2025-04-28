8b85854944991952a898ce14123380ebcda77a5d
Malak Zaidi <malakzaidi815@gmail.com>
2025-04-28 15:46:51 +0000

    switch to the Table per class strategy and visualise it in the database

--- src/main/java/org/springmvc/ebanking/entities/CurrentAccount.java
+++ src/main/java/org/springmvc/ebanking/entities/CurrentAccount.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 
 @Entity
-@DiscriminatorValue("CA")
+//@DiscriminatorValue("CA")
 @Data @NoArgsConstructor @AllArgsConstructor
 public class CurrentAccount extends BankAccount {
     private double overDraft;
