e7eb149d93d6deb842a1ec56e43cb8e2ccb81d61
Malak Zaidi <malakzaidi815@gmail.com>
2025-05-18 17:05:41 +0000

    resolve the bugs

--- src/main/java/org/springmvc/ebanking/web/BankAccountRestAPI.java
+++ src/main/java/org/springmvc/ebanking/web/BankAccountRestAPI.java
@@ -1,5 +1,6 @@
 package org.springmvc.ebanking.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springmvc.ebanking.dtos.*;
 import org.springmvc.ebanking.exceptions.BalanceNotSufficientException;
@@ -9,6 +10,7 @@ import org.springmvc.ebanking.services.BankAccountsService;
 import java.util.List;
 
 @RestController
+@Slf4j
 @CrossOrigin("*")
 public class BankAccountRestAPI {
     private BankAccountsService bankAccountService;
@@ -19,45 +21,57 @@ public class BankAccountRestAPI {
 
     @GetMapping("/accounts/{accountId}")
     public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+        log.info("Fetching bank account: {}", accountId);
         return bankAccountService.getBankAccount(accountId);
     }
+
     @GetMapping("/accounts")
-    public List<BankAccountDTO> listAccounts(){
+    public List<BankAccountDTO> listAccounts() {
+        log.info("Fetching all bank accounts");
         return bankAccountService.bankAccountList();
     }
+
     @GetMapping("/accounts/{accountId}/operations")
-    public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
+    public List<AccountOperationDTO> getHistory(@PathVariable String accountId) {
+        log.info("Fetching history for account: {}", accountId);
         return bankAccountService.accountHistory(accountId);
     }
 
     @GetMapping("/accounts/{accountId}/pageOperations")
     public AccountHistoryDTO getAccountHistory(
             @PathVariable String accountId,
-            @RequestParam(name="page",defaultValue = "0") int page,
-            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
-        return bankAccountService.getAccountHistory(accountId,page,size);
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "5") int size) throws BankAccountNotFoundException {
+        log.info("Fetching paged history for account: {}, page: {}, size: {}", accountId, page, size);
+        return bankAccountService.getAccountHistory(accountId, page, size);
     }
+
     @PostMapping("/accounts/debit")
     public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
-        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        log.info("Debiting account: {}", debitDTO);
+        this.bankAccountService.debit(debitDTO.getAccountId(), debitDTO.getAmount(), debitDTO.getDescription());
         return debitDTO;
     }
+
     @PostMapping("/accounts/credit")
     public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
-        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        log.info("Crediting account: {}", creditDTO);
+        this.bankAccountService.credit(creditDTO.getAccountId(), creditDTO.getAmount(), creditDTO.getDescription());
         return creditDTO;
     }
+
     @PostMapping("/accounts/transfer")
     public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        log.info("Transferring: {}", transferRequestDTO);
         this.bankAccountService.transfer(
                 transferRequestDTO.getAccountSource(),
                 transferRequestDTO.getAccountDestination(),
                 transferRequestDTO.getAmount());
     }
+
     @DeleteMapping("/accounts/{accountId}")
     public void deleteBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+        log.info("Deleting bank account: {}", accountId);
         bankAccountService.deleteBankAccount(accountId);
     }
-
-
 }
--- src/main/java/org/springmvc/ebanking/web/CustomerRestController.java
+++ src/main/java/org/springmvc/ebanking/web/CustomerRestController.java
@@ -1,13 +1,11 @@
 package org.springmvc.ebanking.web;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springmvc.ebanking.dtos.CustomerDTO;
-import org.springmvc.ebanking.entities.Customer;
 import org.springmvc.ebanking.exceptions.CustomerNotFoundException;
 import org.springmvc.ebanking.services.BankAccountsService;
-
-import java.util.List;
 
 import java.util.List;
 
@@ -17,29 +15,41 @@ import java.util.List;
 @CrossOrigin("*")
 public class CustomerRestController {
     private BankAccountsService bankAccountService;
+
     @GetMapping("/customers")
-    public List<CustomerDTO> customers(){
+    public List<CustomerDTO> customers() {
+        log.info("Fetching all customers");
         return bankAccountService.listCustomers();
     }
+
     @GetMapping("/customers/search")
-    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
-        return bankAccountService.searchCustomers("%"+keyword+"%");
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        log.info("Searching customers with keyword: {}", keyword);
+        return bankAccountService.searchCustomers("%" + keyword + "%");
     }
+
     @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
+        log.info("Fetching customer with ID: {}", customerId);
         return bankAccountService.getCustomer(customerId);
     }
+
     @PostMapping("/customers")
-    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        log.info("Saving customer: {}", customerDTO);
         return bankAccountService.saveCustomer(customerDTO);
     }
+
     @PutMapping("/customers/{customerId}")
-    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO){
+    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO) {
+        log.info("Updating customer ID: {} with data: {}", customerId, customerDTO);
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     }
+
     @DeleteMapping("/customers/{id}")
-    public void deleteCustomer(@PathVariable Long id){
+    public void deleteCustomer(@PathVariable Long id) {
+        log.info("Deleting customer with ID: {}", id);
         bankAccountService.deleteCustomer(id);
     }
 }
